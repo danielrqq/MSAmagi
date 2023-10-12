cad21461221fc0112c06649551e6e3eb3a4077fb
Daniel <Daniel.Ryberg@rejlers.se>
2023-10-12 15:46:06 +0200

    ya

--- stream.py
+++ stream.py
@@ -6,6 +6,7 @@ import shutil
 import tempfile
 import zipfile
 
+
 # Streamlit Configuration
 st.set_page_config(
     page_title="MSAmagi",
@@ -14,107 +15,110 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-TEMPLATE_PATH_BASE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mallar')
-RESULTS_DIRECTORY = tempfile.mkdtemp()
+st.markdown("<h1 style='color: teal; font-family: Arial, sans-serif; font-size: 24px; font-weight: bold; text-align: center;'>MSA-hantering</h1>", unsafe_allow_html=True)
 
-def handle_uploaded_zip(uploaded_zip):
-    # Extract the uploaded ZIP to a temporary directory
-    with zipfile.ZipFile(uploaded_zip, 'r') as zip_ref:
-        temp_dir = tempfile.mkdtemp()
-        zip_ref.extractall(temp_dir)
-
-    # Process MSA files in the temporary directory
-    process_msa_files(temp_dir)
-
-    # Clean up (optional)
-    shutil.rmtree(temp_dir)
-
-def zip_results_directory():
-    # Create a separate directory for the zip file
-    zip_dir = tempfile.mkdtemp()
-    zip_filename = os.path.join(zip_dir, "results.zip")
+# Initialize MAIN_TEMP_DIR for the session
+if 'MAIN_TEMP_DIR' not in st.session_state:
+    st.session_state.MAIN_TEMP_DIR = tempfile.mkdtemp()
     
-    # Archive the RESULTS_DIRECTORY
-    shutil.make_archive(zip_filename[:-4], 'zip', RESULTS_DIRECTORY)
-    return zip_filename
+MSA_DIRECTORY = None
+RESULTS_DIRECTORY = None
+MAIN_TEMP_DIR = st.session_state.MAIN_TEMP_DIR
 
-def process_msa_files(msa_directory):
-    # Get the list of files in the directory
-    files_in_directory = os.listdir(msa_directory)
+# Upload File
+uploaded_file = st.file_uploader("Upload Files", type=['zip'])
 
-    # Print the number of files in the directory
-    print(f"Antal filer i {msa_directory}: {len(files_in_directory)}")
-    
-    for filename in os.listdir(msa_directory):
-        if filename.endswith(".xlsx"):
-            msa_path = os.path.join(msa_directory, filename)
-            msa_data = read_msa_info(msa_path)
-            customer = identify_customer(msa_data['customer_name'], msa_data['cell_d4_value'])
-            print_info(msa_path, msa_data, customer)
-            if customer:
-                update_word_template(customer, msa_data)
-                update_prisberakningsmall(customer, msa_data['landlord_desc'], msa_data['tenant_desc'], msa_data)
-            else:
-                print(f"Invalid customer name in MSA file: {msa_path}")
+if uploaded_file:
+    MSA_DIRECTORY = os.path.join(MAIN_TEMP_DIR, "uploaded_files")
+    os.makedirs(MSA_DIRECTORY, exist_ok=True)
+    with zipfile.ZipFile(uploaded_file, 'r') as zip_ref:
+        zip_ref.extractall(MSA_DIRECTORY)
 
-def process_single_msa_file(uploaded_file_content, file_name):
-    with open("temp_msa_file.xlsx", "wb") as f:
-        f.write(uploaded_file_content)
+    RESULTS_DIRECTORY = os.path.join(MAIN_TEMP_DIR, "results")
+    os.makedirs(RESULTS_DIRECTORY, exist_ok=True)
+
+    RESULTS_DIRECTORY = os.path.join(MAIN_TEMP_DIR, "results")
+    os.makedirs(RESULTS_DIRECTORY, exist_ok=True)
+
+saved_template_paths = {}
+
+def handle_uploaded_template(uploaded_file, key):
+    """Save the uploaded template directly to a file."""
+    extension = ".docx" if "Avtalsmall" in key else ".xlsx"
+    template_dir = os.path.join(st.session_state.MAIN_TEMP_DIR, "templates")
+    os.makedirs(template_dir, exist_ok=True)
+    temp_file_path = os.path.join(template_dir, f"{key}{extension}")
     
-    msa_data = read_msa_info("temp_msa_file.xlsx")
-    customer = identify_customer(msa_data['customer_name'], msa_data['cell_d4_value'])
-    print_info(file_name, msa_data, customer)
+    with open(temp_file_path, "wb") as f:
+        f.write(uploaded_file.read())
+
     
-    if customer:
-        print("Processing Avtalsmall...")  # Debugging line
-        update_word_template(customer, msa_data)
-        
-        print("Processing Prisberakningsmall...")  # Debugging line
-        update_prisberakningsmall(customer, msa_data['landlord_desc'], msa_data['tenant_desc'], msa_data)
+    # Save only the filename to the session state
+    if 'uploaded_templates' not in st.session_state:
+        st.session_state.uploaded_templates = {}
+
+    st.session_state.uploaded_templates[key] = f"{key}{extension}"
+
+st.markdown("<h2 style='color: teal; font-family: Arial, sans-serif; font-size: 24px; font-weight: bold; text-align: left;'>Ladda upp mallar nedan</h2>", unsafe_allow_html=True)
+
+# Use containers to visually separate the clients
+for customer in ["3GIS", "Hi3G", "Telia"]:
+    with st.container():
+        st.write(f"**{customer}**")
+        for template_type in ["Avtalsmall", "Prisberakningsmall"]:
+            key = f"{customer}_{template_type}"
+
+            # Only display file uploader if the template hasn't been uploaded yet
+            if key not in st.session_state.get('uploaded_templates', {}):
+                uploaded_template = st.file_uploader(f"Ladda upp {template_type} för {customer}", key=key)
+                if uploaded_template:
+                    handle_uploaded_template(uploaded_template, key)
+
+
+def get_template_path(customer, template_type):
+    """Retrieve the saved template path."""
+    key = f"{customer}_{template_type}"
+    # Generate path on-the-fly
+    if key in st.session_state.uploaded_templates:
+        return os.path.join(MAIN_TEMP_DIR, "templates", st.session_state.uploaded_templates[key])
     else:
-        print(f"Invalid customer name in uploaded MSA file: {file_name}")
+        st.write(f"Please upload the {template_type} for {customer}.")
+        return None
 
-    os.remove("temp_msa_file.xlsx")
-
-
-def read_msa_info(msa_path):
-    wb = openpyxl.load_workbook(msa_path, data_only=True)
-    sheet = wb.active
-    customer_name = sheet["D3"].value
-    cell_d4_value = sheet["D4"].value
-    data = {
-        'customer_name': customer_name,
-        'tenant_desc': get_tenant_description(sheet, customer_name, cell_d4_value),
-        'landlord_desc': get_landlord_description(sheet),
-        'x_coord': get_coordinate(sheet["D10"].value, sheet["D9"].value),
-        'y_coord': get_coordinate(sheet["D11"].value, sheet["D9"].value),
-        'mast_info': sheet["E12"].value,
-        'cell_d4_value': cell_d4_value
-    }
-    wb.close()
-    return data
 
 def get_tenant_description(sheet, customer_name, cell_d4_value):
     customer_name = customer_name.lower() if isinstance(customer_name, str) else str(customer_name)
     cell_d4_value = cell_d4_value.lower() if isinstance(cell_d4_value, str) else str(cell_d4_value)
     if 'telia' in customer_name or 'telia sverige ab' in cell_d4_value:
-        tenant_desc_d6 = sheet["D6"].value
-        if tenant_desc_d6:
-            print(f"For Telia customer, tenant description chosen: {tenant_desc_d6}")
-            return tenant_desc_d6
+        return sheet["D6"].value
     return sheet["D8"].value
 
-def get_landlord_description(sheet):
-    landlord_desc_d4 = sheet["E9"].value
-    if landlord_desc_d4 != "Hyresvärds uppgifter" and landlord_desc_d4:
-        return landlord_desc_d4
-    landlord_desc_e7 = sheet["E7"].value
-    if landlord_desc_e7 != "Hyresvärds uppgifter" and landlord_desc_e7:
-        return landlord_desc_e7
-    return sheet["E9"].value or ""
+def get_landlord_description(sheet, identified_customer):  # added identified_customer parameter
+    if identified_customer == 'Telia':
+        return sheet["E7"].value or ""
+    else:
+        return sheet["E9"].value or ""
 
 def get_coordinate(coord, alternative_coord):
     return coord or alternative_coord
+
+def read_msa_info(msa_path):
+    wb = openpyxl.load_workbook(msa_path, data_only=True)
+    sheet = wb.active
+    identified_customer = identify_customer(sheet["D3"].value, sheet["D4"].value)  # Identifying the customer
+
+    data = {
+        'customer_name': sheet["D3"].value,
+        'tenant_desc': get_tenant_description(sheet, sheet["D3"].value, sheet["D4"].value),
+        'landlord_desc': get_landlord_description(sheet, identified_customer),  # Fetch landlord description based on identified customer
+        'x_coord': get_coordinate(sheet["D10"].value, sheet["D9"].value),
+        'y_coord': get_coordinate(sheet["D11"].value, sheet["D9"].value),
+        'mast_info': sheet["E12"].value,
+        'cell_d4_value': sheet["D4"].value
+    }
+    wb.close()
+    return data
+
 
 def identify_customer(customer_name, cell_d4_value):
     customer_name = customer_name.lower() if isinstance(customer_name, str) else str(customer_name)
@@ -127,18 +131,6 @@ def identify_customer(customer_name, cell_d4_value):
         return 'Telia'
     return None
 
-def print_info(msa_path, msa_data, customer):
-    print(f"Processing file: {msa_path}")
-    for key, value in msa_data.items():
-        print(f"{key.capitalize()}: {value}")
-    print(f"Customer: {customer}")
-    print(f"____________________")
-
-def sanitize_file_name(name):
-    sanitized_name = '_'.join(name.split())
-    sanitized_name = sanitized_name.replace("/", "_")
-    return sanitized_name
-
 def replace_placeholders_in_document(doc, data):
     placeholders = {
         "--::HYRESVARD::--": data['landlord_desc'] or "",
@@ -147,104 +139,118 @@ def replace_placeholders_in_document(doc, data):
         "--::y_coordinate::--": str(data['y_coord']) if data['y_coord'] else "",
         "--::masttyp_hojd::--": str(data['mast_info']) if data['mast_info'] else "",
     }
-
     for paragraph in doc.paragraphs:
         for run in paragraph.runs:
             for key, value in placeholders.items():
                 if key in run.text:
                     run.text = run.text.replace(key, value)
 
-def update_word_template(customer, msa_data):
-    template_path = os.path.join(TEMPLATE_PATH_BASE, customer, f"Avtalsmall_{customer}.docx")
+def sanitize_file_name(name):
+    sanitized_name = '_'.join(name.split())
+    sanitized_name = sanitized_name.replace("/", "_")
+    return sanitized_name
 
-    if os.path.exists(template_path):
+def update_sheet_based_on_customer(sheet, customer, landlord_desc, tenant_desc):
+    if customer == 'Hi3G':
+        sheet['G1'] = landlord_desc
+    elif customer == '3GIS':
+        sheet['D6'] = landlord_desc
+    elif customer == 'Telia':
+        if tenant_desc:
+            sheet['D7'] = tenant_desc
+        sheet['D9'] = landlord_desc
+
+def update_word_template(customer, msa_data):
+    #st.write(f"Checking for Word template of {customer} at: {get_template_path(customer, 'Avtalsmall')}")
+    template_path = get_template_path(customer, "Avtalsmall")
+    if template_path and os.path.exists(template_path):
         doc = Document(template_path)
         replace_placeholders_in_document(doc, msa_data)
-
         result_folder = os.path.join(RESULTS_DIRECTORY, customer)
         os.makedirs(result_folder, exist_ok=True)
-
         sanitized_landlord_desc = sanitize_file_name(msa_data['landlord_desc'])
-        sanitized_tenant_desc = sanitize_file_name(msa_data['tenant_desc'])
-        
-        result_file_name = f"Avtalsmall_{sanitized_landlord_desc}_{sanitized_tenant_desc}_{customer}.docx"
-        result_path = os.path.join(RESULTS_DIRECTORY, customer, result_file_name)
-        
-        doc.save(result_path)
+        output_filename = f"{sanitized_landlord_desc}_Avtalsmall.docx"
+        output_filepath = os.path.join(result_folder, output_filename)
+        doc.save(output_filepath)
+        #st.write(f"Generated document for {customer} in {result_folder}")
     else:
-        print(f"The file {template_path} does NOT exist.")
+        st.write(f"Template for {customer} not found or not uploaded. Check the path: {template_path}")
+
+def update_prisberakningsmall(customer, landlord_desc, tenant_desc, data):
+    #st.write(f"Checking for Prisberakningsmall of {customer} at: {get_template_path(customer, 'Prisberakningsmall')}")
+    template_path = get_template_path(customer, "Prisberakningsmall")
+    try:
+        if template_path and os.path.exists(template_path):
+            #st.write(f"Retrieved path for {customer}'s Prisberäkningsmall: {template_path}")
+
+            wb = openpyxl.load_workbook(template_path)
+            sheet = wb.active
+
+            update_sheet_based_on_customer(sheet, customer, landlord_desc, tenant_desc)  # <-- This line is added.
+
+            result_folder = os.path.join(RESULTS_DIRECTORY, customer)
+            os.makedirs(result_folder, exist_ok=True)
+
+            result_file_name = f"{sanitize_file_name(landlord_desc)}_{sanitize_file_name(tenant_desc)}_{customer}_Prisberakningsmall.xlsx"
+            result_path = os.path.join(result_folder, result_file_name)
+
+            wb.save(result_path)
+            wb.close()
+        else:
+            st.write(f"The file for {customer}'s Prisberäkningsmall does NOT exist at {template_path}.")
+    except Exception as e:
+        st.write(f"Error while processing Prisberäkningsmall for {customer}: {str(e)}")
 
 
-def update_prisberakningsmall(customer, landlord_desc, tenant_desc, msa_data):
-    template_path = os.path.join(TEMPLATE_PATH_BASE, customer, f"Prisberakningsmall_{customer}.xlsx")
-    print(f"Attempting to update Prisberakningsmall with template at {template_path}")  # Debugging line
+def process_msa_files(msa_directory):
+    files_processed = False
+    for filename in os.listdir(msa_directory):
+        if filename.endswith(".xlsx"):
+            msa_path = os.path.join(msa_directory, filename)
+            #st.write(f"Processing MSA file: {filename}")
+            msa_data = read_msa_info(msa_path)
 
-    if os.path.exists(template_path):
-        print("Template exists. Proceeding to update...")  # Debugging line
+             # Log the extracted data
+            #st.write(f"Data from {filename}: {msa_data}")
+            
+            customer = identify_customer(msa_data['customer_name'], msa_data['cell_d4_value'])
+            if customer:
+                update_word_template(customer, msa_data)
+                update_prisberakningsmall(customer, msa_data['landlord_desc'], msa_data['tenant_desc'], msa_data)
+                files_processed = True
+    return files_processed
 
-        # Load the Excel workbook and sheet
-        wb = openpyxl.load_workbook(template_path)
-        sheet = wb.active
+def compress_results_to_zip(results_directory, zip_filename):
+    """Compress all the results into a single .zip file."""
+    with zipfile.ZipFile(zip_filename, 'w') as zipf:
+        for foldername, subfolders, filenames in os.walk(results_directory):
+            for filename in filenames:
+                file_path = os.path.join(foldername, filename)
+                arcname = os.path.relpath(file_path, results_directory)
+                zipf.write(file_path, arcname)
 
-        # Assuming you need to replace placeholders in the Excel sheet as you did in the Word document
-        for row in sheet.iter_rows():
-            for cell in row:
-                for key, value in msa_data.items():
-                    placeholder = f"--::{key.upper()}::--"
-                    if placeholder in str(cell.value or ""):
-                        cell.value = cell.value.replace(placeholder, str(value))
+if st.button("Hantera filer"):
+    with st.spinner('Hanterar filer...'):
+        # Check if all templates are uploaded before processing
+        all_templates_provided = True
+        for customer in ["3GIS", "Hi3G", "Telia"]:
+            for template_type in ["Avtalsmall", "Prisberakningsmall"]:
+                key = f"{customer}_{template_type}"
+                if key not in st.session_state.get('uploaded_templates', {}).keys():
+                    st.write(f"Please upload the {template_type} for {customer}.")
+                    all_templates_provided = False
+        
+        if all_templates_provided:
+            files_processed = process_msa_files(MSA_DIRECTORY)
 
-        # Save the modified Excel workbook
-        result_folder = os.path.join(RESULTS_DIRECTORY, customer)
-        os.makedirs(result_folder, exist_ok=True)
-        sanitized_landlord_desc = sanitize_file_name(landlord_desc)
-        sanitized_tenant_desc = sanitize_file_name(tenant_desc)
-        result_file_name = f"Prisberakningsmall_{sanitized_landlord_desc}_{sanitized_tenant_desc}_{customer}.xlsx"
-        result_path = os.path.join(result_folder, result_file_name)
-        wb.save(result_path)
-        wb.close()
-    else:
-        print(f"The file {template_path} does NOT exist.")  # This will show if the path is incorrect or if the template does not exist
+            
+        if files_processed:
+            zip_filename = os.path.join(MAIN_TEMP_DIR, "processed_results.zip")
+            compress_results_to_zip(RESULTS_DIRECTORY, zip_filename)
+  ###############   
 
-results_directory = RESULTS_DIRECTORY  
-
-choice = st.radio("Önskar du hantera en enskild MSA eller en hel folder?", ["Enskild MSA", "Hel folder"])
-
-# Initialize and set session state variables for msa_directory, results_directory and templates_directory if they don't exist.
-if 'msa_directory' not in st.session_state:
-    st.session_state.msa_directory = RESULTS_DIRECTORY
-
-if 'templates_directory' not in st.session_state:
-    st.session_state.templates_directory = TEMPLATE_PATH_BASE
-
-if choice == "Enskild MSA":
-    uploaded_file = st.file_uploader("Ladda upp MSA nedan.", type=["xlsx"])
-    if uploaded_file:
-        process_single_msa_file(uploaded_file.getvalue(), uploaded_file.name)
-        st.write("MSA hanterad.")
-
-elif choice == "Hel folder":
-    # Use session state variables as default values for the text inputs
-    msa_directory = st.text_input("Ange filsökvägen till MSA-filer:", value=st.session_state.msa_directory)
-    templates_directory = st.text_input("Ange filsökväg där mallar finns:", value=st.session_state.templates_directory)
-
-    # Update session state values based on user input
-    st.session_state.msa_directory = msa_directory
-    st.session_state.templates_directory = templates_directory
-
-    if st.button("Hantera filer"):
-        if msa_directory and results_directory and templates_directory:
-            process_msa_files(msa_directory)
-            st.write("Alla MSA-filer i vald folder har nu blivit hanterade.")
-            zip_file = zip_results_directory()
-
-            with open(zip_file, "rb") as f:
-                bytes_data = f.read()
-                st.download_button(
-                    label="Ladda hem hanterade avtalsmallar och prisberakningsmallar",
-                    data=bytes_data,
-                    file_name="results.zip",
-                    mime="application/zip"
-                )
-
-st.stop()
+            with open(zip_filename, "rb") as f:
+                zip_bytes = f.read()
+                    
+            st.download_button(label="Ladda hem hanterade dokument", data=zip_bytes, mime="application/zip")
+    
